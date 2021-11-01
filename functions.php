c9d8f001bcb60d75b6c99d139ef4d3a01b83ebd8
Kevin Dees <kvdees@gmail.com>
2021-11-01 10:17:47 -0400

    improve default wp settings, styles, scripts, and html

--- functions.php
+++ functions.php
@@ -16,6 +16,11 @@ $manifest = \TypeRocket\Utility\Manifest::cache(
 add_action('wp_enqueue_scripts', function() use ($manifest) {
     wp_enqueue_style( 'main-style', THEME_URI . '/public' . $manifest['/theme/theme.css'] );
     wp_enqueue_script( 'main-script', THEME_URI . '/public' . $manifest['/theme/theme.js'], [], false, true );
+
+    // Threaded comment reply styles.
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 });
 
 // Admin Assets
@@ -24,12 +29,31 @@ add_action('admin_enqueue_scripts', function() use ($manifest) {
     wp_enqueue_script( 'admin-script', THEME_URI . '/public' . $manifest['/admin/admin.js'], [], false, true );
 });
 
-// Supports
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'title-tag' );
+// Navigation
 register_nav_menu( 'main', 'Main Menu' );
 
-// Templates
+// Comments
 apply_filters( 'comments_template', function() {
     return tr_views_path('comments.php');
+});
+
+// Clean Theme Setup
+add_action('after_setup_theme', function() {
+    // Add Supports
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'align-wide' );
+    add_theme_support('html5', ['comment-form', 'comment-list', 'navigation-widgets', 'script', 'style', 'gallery', 'caption']);
+    add_theme_support( 'responsive-embeds' );
+
+    // Thumbnails
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 1568, 9999 );
+
+    // Remove EditURI link
+    remove_action( 'wp_head', 'rsd_link' );
+    // Remove windows live writer
+    remove_action( 'wp_head', 'wlwmanifest_link' );
+    // Remove WP version
+    remove_action( 'wp_head', 'wp_generator' );
 });
--- resources/views/main.php
+++ resources/views/main.php
@@ -1,9 +1,8 @@
 <!doctype html>
-<html class="no-js" <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php wp_head(); ?>
 </head>
@@ -30,5 +29,6 @@
 </main>
 
 <?php wp_footer(); ?>
+
 </body>
 </html>
